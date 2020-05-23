6fe66b5f97e74cda20d5e04475f3b02131341a91
Alexander Lodygin <lodigin.alexander@gmail.com>
2020-05-23 21:59:03 +0300

    fix port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./app')
-const post = process.env.PORT || 5000
+const post = process.env.PORT || 8080
 
 app.listen(post, () => console.log(`Server has been started on ${post}`))
